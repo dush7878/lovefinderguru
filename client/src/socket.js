56a7ebbd954db4ab4bf6815cac03d8e4ef98db19
anil29717 <anil29717@gmail.com>
2025-07-03 23:20:21 +0530

    socket chnage

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000"); // Replace with your backend URL if deployed
+export const socket = io("https://chatsphere-backend-swnr.onrender.com"); // Replace with your backend URL if deployed
