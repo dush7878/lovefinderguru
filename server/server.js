ba7d18226c9b259662820ff181c16e4539aa3abc
dush7878 <solutionaimtech@gmail.com>
2025-07-04 00:17:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://chatsphereapp.vercel.app',
+  origin: 'lovefinderguru.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
