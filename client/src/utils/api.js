29a619a4fd28f6a1662da4a45f922d698e72871c
anil29717 <anil29717@gmail.com>
2025-07-03 22:45:12 +0530

    backend sever

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://chatsphere-backend-swnr.onrender.com',
 });
 
 API.interceptors.request.use((req) => {
