63992392a7c23ff99a3ea544ec0527f9bea2d9fa
dush7878 <solutionaimtech@gmail.com>
2025-07-04 00:14:08 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("https://chatsphere-backend-swnr.onrender.com"); 
+export const socket = io("https://lovefinderguru.onrender.com"); 
