c33d34bcf643a80493eee11ed40ff4c00673d02b
anil29717 <anil29717@gmail.com>
2025-07-03 23:34:42 +0530

    errro backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://chatsphere-backend-swnr.onrender.com',
+  baseURL: 'https://chatsphere-backend-swnr.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
