75927213714e0540e22a27e826c7fe9399f5b240
dush7878 <solutionaimtech@gmail.com>
2025-07-06 18:16:14 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -10,7 +10,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (user) {
-      const newSocket = io('http://localhost:5000');
+      const newSocket = io('https://lovefinderguru.onrender.com');
       newSocket.emit('join', user.id);
       setSocket(newSocket);
 
