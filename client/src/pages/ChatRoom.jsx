53a52b03a793551901c3377aadbff6f9e9d72848
dush7878 <solutionaimtech@gmail.com>
2025-07-06 18:18:06 +0530

    Update ChatRoom.jsx

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -3,7 +3,9 @@ import { useParams, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import API from "../utils/api";
 import { socket } from "../socket";
-
+import { toast, ToastContainer } from "react-toastify";
+import { Send } from "lucide-react";
+import "react-toastify/dist/ReactToastify.css";
 
 const ChatRoom = () => {
   const { user } = useAuth();
